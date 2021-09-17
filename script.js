a6cab8406fce3b9474f5326b3065fe7542fa23f0
Kamila Kryszak <kamilakryszak@yahoo.com>
2021-09-17 20:27:22 +0200

    implementing a tabbed component

--- script.js
+++ script.js
@@ -42,5 +42,30 @@ if (e.target.classList.contains('nav__link')) {
 };
 });
 
+// Tabbed component 
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+
+tabsContainer.addEventListener('click', function(e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+  
+  // Guard clause
+  if (!clicked) return;
+
+  // Removing active classes
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(tab => tab.classList.remove('operations__content--active'))
+
+  //Active tab and content area
+  clicked.classList.add('operations__tab--active');
+
+  console.log(clicked.dataset.tab);
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+})
+
 
 
