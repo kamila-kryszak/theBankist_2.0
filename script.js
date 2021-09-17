8bc0e392d652f3171538b9bf98c5ee50ed5cd12f
Kamila Kryszak <kamilakryszak@yahoo.com>
2021-09-17 23:11:18 +0200

    navigation fade animation

--- script.js
+++ script.js
@@ -51,7 +51,6 @@ const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function(e) {
   const clicked = e.target.closest('.operations__tab');
-  console.log(clicked);
   
   // Guard clause
   if (!clicked) return;
@@ -63,9 +62,33 @@ tabsContainer.addEventListener('click', function(e) {
   //Active tab and content area
   clicked.classList.add('operations__tab--active');
 
-  console.log(clicked.dataset.tab);
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 })
 
 
 
+// Menu fade animation
+
+const nav = document.querySelector('.nav');
+
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    
+    siblings.forEach( el => {
+      if (el !== link) 
+        el.style.opacity = this;
+    })
+      logo.style.opacity = this;
+      }
+}
+
+
+// Passing 'argument' into handler
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+ 
