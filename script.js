e32edd89c9f5bfffff2dcfe7b0bacaa83341cdf0
Kamila Kryszak <kamilakryszak@yahoo.com>
2021-09-19 19:22:25 +0200

    implementing section revelation

--- script.js
+++ script.js
@@ -117,4 +117,27 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe(header);
 
 
- 
+ // The intersection observer API for section reveal
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function( entries, observer) {
+   const [entry] = entries;
+   console.log(entry);
+
+   if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+   };
+
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15
+});
+
+allSections.forEach( function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
